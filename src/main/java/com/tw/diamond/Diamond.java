e54c4de11944be91f928f05e15e80a9868f0fb6f
Xiaojuan Xu <xjxu@thoughtworks.com>
2017-07-15 16:28:26 +0800

    Refactor: Diamond.diamond()

--- src/main/java/com/tw/diamond/Diamond.java
+++ src/main/java/com/tw/diamond/Diamond.java
@@ -1,5 +1,6 @@
 package com.tw.diamond;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,43 @@ public class Diamond {
     }
 
     List<String> diamond(String letter) {
+        ArrayList<String> lines = new ArrayList<String>();
+        for (String lineLetter : getSequence(letter)) {
+            lines.add(generateLine(letter, lineLetter));
+        }
+
+        return lines;
+    }
+
+    private String generateLine(String letter, String lineLetter) {
+        if (letter.equals("A")) {
+            return "A";
+        } else if (letter.equals("B")) {
+            if (lineLetter.equals("A")) {
+                return " A ";
+            } else {
+                return "B B";
+            }
+        } else {
+            if (lineLetter.equals("A")) {
+                return "  A  ";
+            } else if (lineLetter.equals("B")) {
+                return " B B ";
+            } else {
+                return "C   C";
+            }
+        }
+    }
+
+    private List<String> getSequence(String letter) {
         if (letter.equals("A")) {
             return Arrays.asList("A");
+        } else if (letter.equals("B")) {
+            return Arrays.asList("A", "B", "A");
+        } else {
+            return Arrays.asList("A", "B", "C", "B", "A");
         }
-        if (letter.equals("B")) {
-            return Arrays.asList(" A ", "B B", " A ");
-        }
-        return Arrays.asList("  A  ", " B B ", "C   C", " B B ", "  A  ");
     }
+
+
 }
